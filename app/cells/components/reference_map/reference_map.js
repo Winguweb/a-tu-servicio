77ac93d116efc7f028d948292430b06b7784f479
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-07-26 18:26:13 -0300

    mapboxjs with vuejs

--- app/cells/components/reference_map/reference_map.js
+++ app/cells/components/reference_map/reference_map.js
@@ -1,46 +1,42 @@
-ATSB.Components['components/reference-map'] = Backbone.View.extend({
-  initialize: function(options) {
-    _.bindAll(
-      this,
-      'createMap',
-    )
-
-    this.setAccessToken(options.token)
-    this.setMapContainer('#map-container')
-    this.loadDefaults(options)
-    this.createMap()
-    this.centerMap(this.center)
-    this.showReference(this.features)
-  },
-  centerMap: function(center) {
-    this.map.setView([center.lat, center.lng], this.zoom)
-  },
-  createMap: function() {
-    this.map = L.mapbox.map(this.mapContainer[0], this.style)
-    this.baseGeometryFeature = new L.FeatureGroup()
-    this.map.addLayer(this.baseGeometryFeature)
-  },
-  loadDefaults: function(options) {
-    this.center = options.defaults.center
-    this.features = options.features
-    this.style = options.defaults.style
-    this.zoom = options.defaults.zoom
-  },
-  setAccessToken: function(token) {
-    L.mapbox.accessToken = token
-  },
-  setMapContainer: function(selector) {
-    this.mapContainer = this.$el.find(selector)
-  },
-  showReference: function(markers) {
-    markers.forEach(function(marker) {
-      new L.Marker(marker.coordinates, {
-        icon: new L.divIcon({
-          html: '<div class="reference-map--marker"><i></i><p>' + marker.name + '</p></div><!-- Icon made by [https://www.facebook.com/theflaticon] from www.flaticon.com -->',
-          iconAnchor: [8, 15],
-          iconSize: [15, 15],
-        })
-      }).addTo(this.baseGeometryFeature)
-    }.bind(this))
-  },
-})
+ATSB.Components['components/reference-map'] = function(options) {
+  new Vue({
+    el: '.reference-map-cell',
+    data: {
+      branches: options.branches,
+      center: options.defaults.center,
+      style: options.defaults.style,
+      zoom: options.defaults.zoom,
+      token: options.token
+    },
+    mounted: function() {
+      this.setAccessToken()
+      this.createMap()
+      this.centerMap()
+      this.showReference()
+    },
+    methods: {
+      centerMap: function() {
+        this.map.setView([this.center.lat, this.center.lng], this.zoom)
+      },
+      createMap: function() {
+        this.map = L.mapbox.map('map_container', this.style)
+        this.baseGeometryFeature = new L.FeatureGroup()
+        this.map.addLayer(this.baseGeometryFeature)
+      },
+      setAccessToken: function() {
+        L.mapbox.accessToken = this.token
+      },
+      showReference: function() {
+        this.branches.forEach(function(branch) {
+          new L.Marker(branch.coordinates, {
+            icon: new L.divIcon({
+              html: '<div class="reference-map--marker"><i></i><p>' + branch.name + '</p></div><!-- Icon made by [https://www.facebook.com/theflaticon] from www.flaticon.com -->',
+              iconAnchor: [8, 15],
+              iconSize: [15, 15],
+            })
+          }).addTo(this.baseGeometryFeature)
+        }.bind(this))
+      },
+    }
+  })
+}
