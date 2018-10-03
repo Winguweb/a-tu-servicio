ed25786fb860f74913a0ce74ded3c5e0e3d902ef
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-10-03 14:49:25 -0300

    separated yml for vote data

--- app/cells/components/vote_modal/vote_modal.js
+++ app/cells/components/vote_modal/vote_modal.js
@@ -4,60 +4,7 @@ ATSB.Components['components/vote-modal'] = function(options) {
     data: {
       branchId: 1,
       actions: {show: false},
-      steps: [
-        {
-          question: "¿Qué tipo de usuario eres?",
-          answers: [
-            {
-              id: 1, text: "No asegurado"
-            },
-            {
-              id: 2, text: "Subsidiado"
-            },
-            {
-              id: 3, text: "Contributivo"
-            },
-            {
-              id: 4, text: "Particular"
-            },
-          ],
-          answer: null
-        },
-        {
-          question: "Selecciona el servicio para el cual solicitaste la cita?",
-          answers: [
-            {
-              id: 1, text: "Pediatría"
-            },
-            {
-              id: 2, text: "Medicina Interna"
-            },
-            {
-              id: 3, text: "Odontología"
-            },
-            {
-              id: 4, text: "Medicina General"
-            },
-            {
-              id: 5, text: "Ginecología"
-            },
-            {
-              id: 6, text: "Obstetricia"
-            },
-            {
-              id: 7, text: "Cirujía General"
-            },
-            {
-              id: 8, text: "Laboratorio"
-            },
-            {
-              id: 9, text: "Radiología"
-            },
-
-          ],
-          answer: null
-        }
-      ],
+      steps: options.steps,
       actualStep: 0
     },
     created: function() {
