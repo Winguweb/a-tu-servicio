edbeb4a66a95d79e15d7f2095490c2c12e9cd903
Agustin Cavilliotti <cavi21@gmail.com>
2018-10-15 00:29:46 -0300

    Some cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -10,19 +10,12 @@ Rails.application.routes.draw do
   # ---
   match '/(departamento/:departamento)' => 'home#show', via: :get
 
-  get '/comparar' => 'compare#index'
-  get '/comparar/:selected_providers' => 'compare#index', as: :compare
-  get '/agregar' => 'compare#add'
-
   resources :providers do
     get :autocomplete_provider_search_name, on: :collection
   end
 
   get '/sobre-el-proyecto' => 'home#about', as: :about
   get '/mapa-de-servicios' => 'home#services', as: :services
-
-  get '/404' => 'errors#not_found'
-  get '/500' => 'errors#internal_server_error'
 
   get '/apple-touch-icon-precomposed.png', to: redirect('/assets/apple-touch-icon-precomposed.png')
 end
