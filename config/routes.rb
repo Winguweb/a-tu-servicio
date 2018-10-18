f88d9e7aa564ea517fdc2ef63aef6ba6828c654a
rockarloz <rockarlos@me.com>
2018-10-18 12:47:37 -0500

    add Satisfaction csv download

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   get '/sobre-el-proyecto' => 'home#about', as: :about
   get '/mapa-de-servicios' => 'home#services', as: :services
-  get '/datos/descarga/:model', action: :download, controller: :home, as: :donations_download, constraints: { model: /(Branch|Provider)/ }
+  get '/datos/descarga/:model', action: :download, controller: :home, as: :donations_download, constraints: { model: /(Branch|Provider|Satisfaction)/ }
    
 
   get '/apple-touch-icon-precomposed.png', to: redirect('/assets/apple-touch-icon-precomposed.png')
