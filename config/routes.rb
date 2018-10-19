7a0c8f0fa5abcd64efcc023edd312a2089a26aec
rockarloz <rockarlos@me.com>
2018-10-19 07:30:29 -0500

    added wating time download data

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,9 @@ Rails.application.routes.draw do
   resources :providers do
     get :autocomplete_provider_search_name, on: :collection
   end
-
   get '/sobre-el-proyecto' => 'home#about', as: :about
   get '/mapa-de-servicios' => 'home#services', as: :services
-  get '/datos/descarga/:model', action: :download, controller: :home, as: :donations_download, constraints: { model: /(Branch|Provider|Satisfaction|Speciality)/ }
+  get '/datos/descarga/:model', action: :download, controller: :home, as: :donations_download, constraints: { model: /(Branch|Provider|Satisfaction|Speciality|WaitingTime)/ }
    
 
   get '/apple-touch-icon-precomposed.png', to: redirect('/assets/apple-touch-icon-precomposed.png')
