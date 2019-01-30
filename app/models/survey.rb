9cf57caf09af3ec3da3f1fcbbf1ad44aac5fd458
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2019-01-30 16:58:07 -0300

    five stars for all survey main sections

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,6 +3,8 @@ class Survey < ActiveRecord::Base
   default_scope { order(created_at: :asc) }
   after_save :update_quality
   after_save :update_waiting_times
+  after_save :update_satisfaction
+  after_save :update_humanization
 
   private
 
@@ -14,5 +16,15 @@ class Survey < ActiveRecord::Base
   def update_waiting_times
     return unless step_id == 5
     CalculateWaitingTimesWorker.perform_async(branch_id)
+  end
+
+  def update_satisfaction
+    return unless step_id == 7
+    CalculateSatisfactionWorker.perform_async(branch_id)
+  end
+
+  def update_humanization
+    return unless step_id == 12
+    CalculateHumanizationWorker.perform_async(branch_id)
   end
 end
