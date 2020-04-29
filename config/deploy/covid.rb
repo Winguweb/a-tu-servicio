e93d8ce107350fb321f87a9622c6ced3a9c82f53
juandans01 <juandans01@gmail.com>
2020-04-29 16:17:14 -0300

    add covid deploy capistrano config file

---
+++ config/deploy/covid.rb
@@ -0,0 +1,9 @@
+set :stage, :staging
+set :branch, 'covid-19'
+
+server '34.224.95.156', user: 'deploy', roles: %w{ app }
+
+set :rails_env, :production
+set :puma_env, :production
+set :puma_config_file, "#{shared_path}/config/puma.rb"
+set :puma_conf, "#{shared_path}/config/puma.rb"
