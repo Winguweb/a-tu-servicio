8e22622f268ee024e0e7cfab37abe5a04c2cc521
juandans01 <juandans01@gmail.com>
2020-06-05 17:46:48 -0300

    search on home structure

---
+++ app/cells/components/search_bar/search_bar.js
@@ -0,0 +1,5 @@
+ATSB.Components['components/search-bar'] = function(options) {
+  new Vue({
+    el: '.search-bar-cell'   
+  })
+}
--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,7 @@ module AtuservicioRails
       components/splash_cell
       components/story_slider_cell
       components/vote_modal_cell
+      components/search_bar_cell
       mobile_components/m_drawer_cell
       mobile_components/m_site_header_cell
       visualization_components/beds_visualization_cell
