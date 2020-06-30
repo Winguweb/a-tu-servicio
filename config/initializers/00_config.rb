2cd0dfc13fcaadbe4398f48a851ce20436f9560d
juandans01 <juandans01@gmail.com>
2020-06-30 10:48:23 -0300

    remove keys from 00_config

--- config/initializers/00_config.rb
+++ config/initializers/00_config.rb
@@ -1,33 +1,31 @@
 env = ENV['RAILS_ENV'] || Rails.env
 
-# vuejs = APP_CONFIG[env]['vuejs'].freeze	# vuejs = APP_CONFIG[env]['vuejs'].freeze
+# vuejs = APP_CONFIG[env]['vuejs'].freeze
 VUEJS_CDN_URL = ENV['VUE_CDN']
 
-# algolia = APP_CONFIG[env]['algolia'].freeze	# algolia = APP_CONFIG[env]['algolia'].freeze
+# algolia = APP_CONFIG[env]['algolia'].freeze
 ALGOLIA_APP_ID = ENV['ALGOLIA_APP_KEY']
 ALGOLIA_ADMIN_API_KEY = ENV['ALGOLIA_ADMIN_KEY']
 ALGOLIA_SEARCH_API_KEY = ENV['ALGOLIA_SEARCH_KEY']
 
-# google = APP_CONFIG[env]['google'].freeze	# google = APP_CONFIG[env]['google'].freeze
+# google = APP_CONFIG[env]['google'].freeze
 
-# ANALYTICS_TRACKING_ID = google['analytics'].freeze	# ANALYTICS_TRACKING_ID = google['analytics'].freeze
-# GEOCODING_API_KEY = google['geocoding']['api_key']	# GEOCODING_API_KEY = google['geocoding']['api_key']
-# RECAPTCHA_SITEKEY = google['recaptcha']['sitekey']	# RECAPTCHA_SITEKEY = google['recaptcha']['sitekey']
-# RECAPTCHA_SECRETKEY = google['recaptcha']['secretkey']	# RECAPTCHA_SECRETKEY = google['recaptcha']['secretkey']
-# RECAPTCHA_VERIFY_URL = google['recaptcha']['verifyUrl']	# RECAPTCHA_VERIFY_URL = google['recaptcha']['verifyUrl']
+# ANALYTICS_TRACKING_ID = google['analytics'].freeze
+# GEOCODING_API_KEY = google['geocoding']['api_key']
+# RECAPTCHA_SITEKEY = google['recaptcha']['sitekey']
+# RECAPTCHA_SECRETKEY = google['recaptcha']['secretkey']
+# RECAPTCHA_VERIFY_URL = google['recaptcha']['verifyUrl']
 ANALYTICS_TRACKING_ID = ENV['GOOGLE_ANALYTICS']
 GEOCODING_API_KEY = ENV['GOOGLE_GEOCODING']
-RECAPTCHA_SITEKEY = ENV['GOOGLE_SITEKEY']	
-RECAPTCHA_SECRETKEY = ENV['GOOGLE_CAPTCHA_SECRETKEY']	
+RECAPTCHA_SITEKEY = ENV['GOOGLE_SITEKEY']
+RECAPTCHA_SECRETKEY = ENV['GOOGLE_CAPTCHA_SECRETKEY']
 RECAPTCHA_VERIFY_URL = ENV['GOOGLE_RECAPTCHA_VERIFY_URL']
 
-
-# ENV['GOOGLE_ANALYTICS']	# ENV['GOOGLE_ANALYTICS']
-# redis = APP_CONFIG[env]['redis'].freeze	# redis = APP_CONFIG[env]['redis'].freeze
+# ENV['GOOGLE_ANALYTICS']
+# redis = APP_CONFIG[env]['redis'].freeze
 REDIS_DB              = ENV['REDIS_DB']
 REDIS_HOST            = ENV['REDIS_HOST']
 REDIS_PORT            = ENV['REDIS_PORT']
-REDIS_URL             = "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}".freeze	
-
+REDIS_URL             = "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}".freeze
 
 MAPBOX_TOKEN    = ENV['MAPBOX_TOKEN']
