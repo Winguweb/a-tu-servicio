fac0a8991d56aa07b4e55811defc641bd77d9af4
juandans01 <juandans01@gmail.com>
2020-06-30 10:45:12 -0300

    remove keys from 00_config

--- config/initializers/00_config.rb
+++ config/initializers/00_config.rb
@@ -1,30 +1,36 @@
-env = ENV['RAILS_ENV'] || Rails.env
+env = ENV['RAILS_ENV'] || Rails.env	env = ENV['RAILS_ENV'] || Rails.env
 
-# vuejs = APP_CONFIG[env]['vuejs'].freeze
-VUEJS_CDN_URL = "https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"
 
-# algolia = APP_CONFIG[env]['algolia'].freeze
-ALGOLIA_APP_ID = 'ZRACX01RVB'
-ALGOLIA_ADMIN_API_KEY = 'a84823f89a5f9ef1769f901882eff054'
-ALGOLIA_SEARCH_API_KEY = 'd40fac79193db8e7fbe1f1ccd323d2e4'
+# vuejs = APP_CONFIG[env]['vuejs'].freeze	# vuejs = APP_CONFIG[env]['vuejs'].freeze
+VUEJS_CDN_URL = ENV['VUE_CDN']
 
-# google = APP_CONFIG[env]['google'].freeze
-# ANALYTICS_TRACKING_ID = google['analytics'].freeze
-# GEOCODING_API_KEY = google['geocoding']['api_key']
-# RECAPTCHA_SITEKEY = google['recaptcha']['sitekey']
-# RECAPTCHA_SECRETKEY = google['recaptcha']['secretkey']
-# RECAPTCHA_VERIFY_URL = google['recaptcha']['verifyUrl']
-ANALYTICS_TRACKING_ID = ''
-GEOCODING_API_KEY = ''
-RECAPTCHA_SITEKEY = ''
-RECAPTCHA_SECRETKEY = ''
-RECAPTCHA_VERIFY_URL = ''
 
-# ENV['GOOGLE_ANALYTICS']
-# redis = APP_CONFIG[env]['redis'].freeze
-REDIS_DB              = 0
-REDIS_HOST            = 'localhost'
-REDIS_PORT            = 6379
-REDIS_URL             = "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}".freeze
+# algolia = APP_CONFIG[env]['algolia'].freeze	# algolia = APP_CONFIG[env]['algolia'].freeze
+ALGOLIA_APP_ID = ENV['ALGOLIA_APP_KEY']
+ALGOLIA_ADMIN_API_KEY = ENV['ALGOLIA_ADMIN_KEY']
+ALGOLIA_SEARCH_API_KEY = ENV['ALGOLIA_SEARCH_KEY']
 
-MAPBOX_TOKEN = 'pk.eyJ1IjoianVhbmxhY3VldmEiLCJhIjoiY2s1MnBibDltMDA5NTNwbnRwOTM0cnhkdCJ9.17QsP4HUNocfKFxd_Cswuw'
+
+# google = APP_CONFIG[env]['google'].freeze	# google = APP_CONFIG[env]['google'].freeze
+
+# ANALYTICS_TRACKING_ID = google['analytics'].freeze	# ANALYTICS_TRACKING_ID = google['analytics'].freeze
+# GEOCODING_API_KEY = google['geocoding']['api_key']	# GEOCODING_API_KEY = google['geocoding']['api_key']
+# RECAPTCHA_SITEKEY = google['recaptcha']['sitekey']	# RECAPTCHA_SITEKEY = google['recaptcha']['sitekey']
+# RECAPTCHA_SECRETKEY = google['recaptcha']['secretkey']	# RECAPTCHA_SECRETKEY = google['recaptcha']['secretkey']
+# RECAPTCHA_VERIFY_URL = google['recaptcha']['verifyUrl']	# RECAPTCHA_VERIFY_URL = google['recaptcha']['verifyUrl']
+ANALYTICS_TRACKING_ID = ENV['GOOGLE_ANALYTICS']
+GEOCODING_API_KEY = ENV['GOOGLE_GEOCODING']
+RECAPTCHA_SITEKEY = ENV['GOOGLE_SITEKEY']	
+RECAPTCHA_SECRETKEY = ENV['GOOGLE_CAPTCHA_SECRETKEY']	
+RECAPTCHA_VERIFY_URL = ENV['GOOGLE_RECAPTCHA_VERIFY_URL']
+
+
+# ENV['GOOGLE_ANALYTICS']	# ENV['GOOGLE_ANALYTICS']
+# redis = APP_CONFIG[env]['redis'].freeze	# redis = APP_CONFIG[env]['redis'].freeze
+REDIS_DB              = ENV['REDIS_DB']
+REDIS_HOST            = ENV['REDIS_HOST']
+REDIS_PORT            = ENV['REDIS_PORT']
+REDIS_URL             = "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}".freeze	
+
+
+MAPBOX_TOKEN    = ENV['MAPBOX_TOKEN']
