a2346f3dfca87b6274688399c65dd4a5507728c8
juandans01 <juandans01@gmail.com>
2020-07-24 11:52:44 -0300

    changes on csv/xsls export

--- app/services/reporting/survey_exporter.rb
+++ app/services/reporting/survey_exporter.rb
@@ -1,6 +1,6 @@
 module Reporting
   class SurveyExporter < Exporter
-
+    SURVEY_LENGTH = 46
     SURVEY_COLUMNS = [
       { id: 1 },
       { id: 2 },
@@ -91,17 +91,22 @@ module Reporting
       return @csv_header if defined?(@csv_header)
 
       @csv_header = %w[provider_name branch_name]
+      
+      # SURVEY_COLUMNS.each do |column|
+        # next if column[:user_only] && !user_logged_in?
 
-      SURVEY_COLUMNS.each do |column|
-        next if column[:user_only] && !user_logged_in?
-
-        label = if column.key?(:custom_label)
-          column[:custom_label]
-        else
-          $survey_data.steps_labels[column[:id]]
-        end
-        @csv_header << label
+        # label = if column.key?(:custom_label)
+        #   column[:custom_label]
+        # else
+        # end
+        
+        # @csv_header << label
+      # end
+      for i in 1..SURVEY_LENGTH
+        puts i
+        @csv_header << $survey_data.steps_labels[i]
       end
+      puts @csv_header
       @csv_header
     end
 
@@ -110,16 +115,23 @@ module Reporting
 
       @xlsx_header = ["Nombre del Prestador", "Nombre de la Sede"]
 
-      SURVEY_COLUMNS.each do |column|
-        next if column[:user_only] && !user_logged_in?
+      # SURVEY_COLUMNS.each do |column|
+      #   next if column[:user_only] && !user_logged_in?
 
-        label = if column.key?(:custom_label)
-          column[:custom_label]
-        else
-          $survey_data.steps_labels[column[:id]]
-        end
-        @xlsx_header << label
+      #   label = if column.key?(:custom_label)
+      #     column[:custom_label]
+      #   else
+      #     $survey_data.steps_labels[column[:id]]
+      #   end
+      #   @xlsx_header << label
+      # end
+      
+
+      for i in 1..SURVEY_LENGTH
+        puts i
+        @xlsx_header << $survey_data.steps_labels[i]
       end
+      
       @xlsx_header
     end
 
@@ -140,17 +152,17 @@ module Reporting
 
     def yielder
       @surveys.group_by{|s| [s.client_id, s.branch]}.each do |(_, branch), survey_responses|
-        process_survey_responses(survey_responses).each do |row_responses|
-          yield row_data(branch, row_responses)
-        end
+        yield row_data(branch, survey_responses)
+        # process_survey_responses(survey_responses).each do |row_responses|
+        # end
       end
     end
 
     def process_survey_responses(survey_responses)
       # TO-DO: I harcoded this in here because is the only step that has this situation
-      multiple_responses_entry_step_id = 11
-      multiple_responses_group = [ 11, 12, 13, 14 ]
-      responses_other_step_id = [ 19, 20, 21, 22, 23, 24 ]
+      multiple_responses_entry_step_id = 0
+      multiple_responses_group = [ ]
+      responses_other_step_id = [ ]
 
       multiple_responses_columns = survey_responses.select do |response|
         multiple_responses_group.include?(response.step_id)
@@ -166,6 +178,15 @@ module Reporting
         end
       end
 
+      common_colums_data = survey_responses.each_with_object({}) do |response, _hash|
+        _hash[response.step_id] = if responses_other_step_id.include?(response.step_id)
+          response.answer_data['value']
+        else
+          response.answer_data['label']
+        end
+      end
+
+      # puts common_colums_data
       # TO-DO: All this logic below is very order sensitive of the records
       # the assumptions is that the grouped records are next to each other
       response_group = {}
@@ -182,34 +203,55 @@ module Reporting
         response_group[response.step_id] = response.answer_data['label']
       end
       multiple_columns_groups.push(response_group) if within_group
-      # END | TO-DO
+      # # END | TO-DO
 
       multiple_columns_groups.map do |group|
         common_colums_data.merge(group)
       end
+      return common_colums_data
     end
 
-    def row_data(branch, row_responses)
+    def row_data(branch, survey_responses)
       row_colums = [ branch.provider.name, branch.name ]
+      # responses_ids = row_responses.keys
 
-      responses_ids = row_responses.keys
+      # responses_columns = SURVEY_COLUMNS.map do |column|
+      #   # next if column[:user_only] && !user_logged_in?
 
-      responses_columns = SURVEY_COLUMNS.map do |column|
-        next if column[:user_only] && !user_logged_in?
+      #   id = if column[:id].is_a?(Array)
+      #     column[:id].detect{ |id| row_responses[id] }
+      #   end
+      #   column[:id]
 
-        id = if column[:id].is_a?(Array)
-          column[:id].detect{ |id| row_responses[id] }
-        else
-          column[:id]
+      #   # default_value = if row_responses[id].blank? && column.key?(:default_value_ids)
+      #   #   column[:default_value] if (column[:default_value_ids] & responses_ids).present?
+      #   # end
+      #   puts row_responses[id]
+      #   row_colums.push(row_responses[id] || '')
+      # end
+
+      # id = if column[:id].is_a?(Array)
+      #   column[:id].detect{ |id| row_responses[id] }
+      # end
+      # column[:id]
+
+      # default_value = if row_responses[id].blank? && column.key?(:default_value_ids)
+      #   column[:default_value] if (column[:default_value_ids] & responses_ids).present?
+      # end
+
+      # row_colums.push(row_responses[id] || '')
+
+
+      for i in 1..SURVEY_LENGTH
+        value = ' '
+        survey_responses.each_with_object({}) do |response, _hash|          
+          if (response[:step_id].to_i == i)
+            value = response['answer_data']['label']    
+          end
         end
-
-        default_value = if row_responses[id].blank? && column.key?(:default_value_ids)
-          column[:default_value] if (column[:default_value_ids] & responses_ids).present?
-        end
-
-        row_colums.push(row_responses[id] || default_value)
-      end
-
+        row_colums.push(value)        
+      end      
+      
       row_colums
     end
   end
