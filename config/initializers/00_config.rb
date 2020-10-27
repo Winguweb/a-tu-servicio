c57a90a95ff08613c6b0e2aa12c1ea7222dee34a
juandans01 <juandans01@gmail.com>
2020-10-27 11:26:35 -0300

    rollback 00_config

--- config/initializers/00_config.rb
+++ config/initializers/00_config.rb
@@ -1,35 +1,28 @@
 env = ENV['RAILS_ENV'] || Rails.env
 
-# vuejs = APP_CONFIG[env]['vuejs'].freeze
-VUEJS_CDN_URL = ENV['VUE_CDN']
+vuejs = APP_CONFIG[env]['vuejs'].freeze
+VUEJS_CDN_URL = vuejs['cdn_url']
 
-# algolia = APP_CONFIG[env]['algolia'].freeze
-ALGOLIA_APP_ID = ENV['ALGOLIA_APP_KEY']
-ALGOLIA_ADMIN_API_KEY = ENV['ALGOLIA_ADMIN_KEY']
-ALGOLIA_SEARCH_API_KEY = ENV['ALGOLIA_SEARCH_KEY']
+algolia = APP_CONFIG[env]['algolia'].freeze
+ALGOLIA_APP_ID = algolia['application_id'].freeze
+ALGOLIA_ADMIN_API_KEY = algolia['admin_api_key'].freeze
+ALGOLIA_SEARCH_API_KEY = algolia['search_api_key'].freeze
 
-# google = APP_CONFIG[env]['google'].freeze
+google = APP_CONFIG[env]['google'].freeze
+ANALYTICS_TRACKING_ID = google['analytics'].freeze
+GEOCODING_API_KEY = google['geocoding']['api_key']
+USE_RECAPTCHA = google['recaptcha']['use']
+RECAPTCHA_SITEKEY = google['recaptcha']['sitekey']
+RECAPTCHA_SECRETKEY = google['recaptcha']['secretkey']
+RECAPTCHA_VERIFY_URL = google['recaptcha']['verifyUrl']
 
-# ANALYTICS_TRACKING_ID = google['analytics'].freeze
-# GEOCODING_API_KEY = google['geocoding']['api_key']
-# RECAPTCHA_SITEKEY = google['recaptcha']['sitekey']
-# RECAPTCHA_SECRETKEY = google['recaptcha']['secretkey']
-# RECAPTCHA_VERIFY_URL = google['recaptcha']['verifyUrl']
-ANALYTICS_TRACKING_ID = ENV['GOOGLE_ANALYTICS']
-GEOCODING_API_KEY = ENV['GOOGLE_GEOCODING']
-RECAPTCHA_SITEKEY = ENV['GOOGLE_SITEKEY']
-RECAPTCHA_SECRETKEY = ENV['GOOGLE_CAPTCHA_SECRETKEY']
-RECAPTCHA_VERIFY_URL = ENV['GOOGLE_RECAPTCHA_VERIFY_URL']
-USE_RECAPTCHA = ENV['USE_RECAPTCHA'] 
-
-# ENV['GOOGLE_ANALYTICS']
-# redis = APP_CONFIG[env]['redis'].freeze
-REDIS_DB              = ENV['REDIS_DB']
-REDIS_HOST            = ENV['REDIS_HOST']
-REDIS_PORT            = ENV['REDIS_PORT']
+redis = APP_CONFIG[env]['redis'].freeze
+REDIS_DB              = redis['db'].freeze
+REDIS_HOST            = redis['host'].freeze
+REDIS_PORT            = redis['port'].freeze
 REDIS_URL             = "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}".freeze
 
-MAPBOX_TOKEN    = ENV['MAPBOX_TOKEN']
+MAPBOX_TOKEN    = APP_CONFIG[env]['mapbox']['token'].freeze
 # env = ENV['RAILS_ENV'] || Rails.env
 # # vuejs = APP_CONFIG[env]['vuejs'].freeze
 # VUEJS_CDN_URL = "https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"
